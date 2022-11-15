defd4d5d7700ae4a5dc9e3c769b0eee9429a13ac
mrcaseyrobison <mrcaseyrobison@gmail.com>
2022-11-15 02:45:17 -0500

    project page

---
+++ my-portfolio/src/components/Project.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Grid } from '@mui/material';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+
+const styles = {
+    box: {
+        border: "1px solid #007b6d",
+        backgroundColor: "#000000",
+        color: "#D3D3D3"
+    }
+};
+
+export default function Project({ id, title, description, image, repo  }) {
+    return (
+        <Grid item xs={10} md={6} key={id} className="project">
+            <Card style={styles.box}>
+        <Box sx={{ position: 'relative' }}>
+          <CardMedia
+            component="img"
+            height="100%"
+            image={image}
+          />
+          <Box
+            sx={{
+              position: 'absolute',
+              bottom: "0%",
+              left: 0,
+              width: '100%',
+              bgcolor: 'rgba(0, 0, 0, 0.70)',
+              color: '#D3D3D3',
+              padding: '10px',
+            }}
+          >
+            <Typography variant="h5">{title}</Typography>
+            <Typography variant="body2">{description}</Typography>
+
+            <Grid container spacing={4} justifyContent="center">
+              <Grid item>
+                {/* refer later if breaking  */}
+                <a href={repo} target="_blank" rel="noreferrer">GitHub Repo</a>
+              </Grid>
+            </Grid>
+
+          </Box>
+        </Box>
+      </Card>
+    </Grid>
+    )
+}
